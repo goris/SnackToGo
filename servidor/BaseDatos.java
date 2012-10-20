5877c1e23cc41e49cb80dece9672dd53bd18953d
juve <juve-guz@gmail.com>
2012-10-20 18:27:09 -0500

    conexion a la BD

--- servidor/BaseDatos.java
+++ servidor/BaseDatos.java
@@ -1,17 +1,34 @@
 import java.io.*;
 import java.sql.*;
 import java.util.LinkedList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 class BaseDatos{
-	Pedido pedido;
-	private Connection conect;//inicio de conexion
-	private PreparedStatement pstmt=null;
-	private String url="jdbc:mysql://localhost:8889/snack-to-go";
-	private String usuario="root";
-	private String clave="root";
-	public Boolean borrar(Pedido pedido){
-		conect=DriverManager.getConection(url,usuario,clave);
-		stmt=conect.createStatement();
-		pstmt=conect.prepareStatement("DELETE FROM ")
+	private static Connection conect;//inicio de conexion
+	private static PreparedStatement pstmt=null;
+	private static String url="jdbc:mysql://localhost:8889/snack-to-go";
+	private static String usuario="root";
+	private static String clave="root";
+	private static boolean actualiza=true; 
+	public static boolean actualizar(Pedido pedido){
+		try{
+		conect=DriverManager.getConnection(url,usuario,clave);
+		pstmt=conect.prepareStatement("INSERT INTO LineasPedido VALUES(NULL,) ");
+		pstmt=conect.prepareStatement("INSERT INTO Pedidos VALUES(NULL,) ");
+		
+		}catch(SQLException e){
+			System.out.println("se te frego por:"+e.getMessage());
+			actualiza=false;
+		}finally{
+			try{
+			pstmt.close();
+		}catch(java.sql.SQLException sql){
+			System.out.println("se te frego por:"+sql.getMessage());
+		}
+			return actualiza;
+		}
 	}	
 }
