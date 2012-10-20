ca62fba30577f8e0fbdd872f982c2d3227049ee1
Carlos <cacmartinez@msn.com>
2012-10-20 16:42:57 -0500

    creando ios

---
+++ SnackToGoiOS/Prueba/Prueba/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Prueba
+//
+//  Created by Compean on 10/20/12.
+//  Copyright (c) 2012 Compean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnackToGoiOS/Prueba/Prueba/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Prueba
+//
+//  Created by Compean on 10/20/12.
+//  Copyright (c) 2012 Compean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
