974e2aa6d613a11bdf6fef3971da70ea2bae5cf7
David Guzman Araiza <dajavax@gmail.com>
2012-10-21 02:41:33 -0500

    Se cambia el menu de login para consistencia con iOS

--- SnackToGoAndroid/src/com/nutellabread/snacktogo/SelectCafeteria.java
+++ SnackToGoAndroid/src/com/nutellabread/snacktogo/SelectCafeteria.java
@@ -10,7 +10,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class SelectCafeteria extends ListActivity {
 
