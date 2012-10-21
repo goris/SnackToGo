89accf742a951c14862a5ce8e48ef548dbce1fa2
David Guzman Araiza <dajavax@gmail.com>
2012-10-21 06:10:18 -0500

    Terminada la actividad de generar pedido.

---
+++ SnackToGoAndroid/src/com/nutellabread/snacktogo/ListArticuloAdapter.java
@@ -0,0 +1,300 @@
+package com.nutellabread.snacktogo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.ref.SoftReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+ 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+ 
+public class ListArticuloAdapter extends BaseAdapter {
+ 
+    private Activity activity;
+    private ArrayList<HashMap<String, String>> data;
+    private static LayoutInflater inflater=null;
+    public ImageLoader imageLoader; 
+ 
+    public ListArticuloAdapter(Activity a, ArrayList<HashMap<String, String>> d) {
+        activity = a;
+        data=d;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        imageLoader=new ImageLoader(activity.getApplicationContext());
+    }
+ 
+    public int getCount() {
+        return data.size();
+    }
+ 
+    public Object getItem(int position) {
+        return position;
+    }
+ 
+    public long getItemId(int position) {
+        return position;
+    }
+ 
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi=convertView;
+        if(convertView==null)
+            vi = inflater.inflate(R.layout.list_articulo, null);
+ 
+        TextView nombre = (TextView)vi.findViewById(R.id.nombre); // title
+        TextView descripcion = (TextView)vi.findViewById(R.id.descripcion); // artist name
+        TextView precio = (TextView)vi.findViewById(R.id.precio); // duration
+        TextView cantidad = (TextView)vi.findViewById(R.id.cantidad); // duration
+        TextView total = (TextView)vi.findViewById(R.id.total); // duration
+        ImageView thumb_image=(ImageView)vi.findViewById(R.id.list_image); // thumb image
+ 
+        HashMap<String, String> song = new HashMap<String, String>();
+        song = data.get(position);
+ 
+        // Setting all values in listview
+        nombre.setText(song.get(SelectPedido.KEY_NOMBRE));
+        descripcion.setText(song.get(SelectPedido.KEY_DESCRIPCION));
+        precio.setText(song.get(SelectPedido.KEY_PRECIO));
+        cantidad.setText(song.get(SelectPedido.KEY_CANTIDAD));
+        total.setText(song.get(SelectPedido.KEY_TOTAL));
+        imageLoader.DisplayImage(song.get(SelectPedido.KEY_THUMB_URL), thumb_image);
+        return vi;
+    }
+}
+class ImageLoader {
+	 
+    MemoryCache memoryCache=new MemoryCache();
+    FileCache fileCache;
+    private Map<ImageView, String> imageViews=Collections.synchronizedMap(new WeakHashMap<ImageView, String>());
+    ExecutorService executorService; 
+ 
+    public ImageLoader(Context context){
+        fileCache=new FileCache(context);
+        executorService=Executors.newFixedThreadPool(5);
+    }
+ 
+    final int stub_id = R.drawable.gradient_bg;
+    public void DisplayImage(String url, ImageView imageView)
+    {
+        imageViews.put(imageView, url);
+        Bitmap bitmap=memoryCache.get(url);
+        if(bitmap!=null)
+            imageView.setImageBitmap(bitmap);
+        else
+        {
+            queuePhoto(url, imageView);
+            imageView.setImageResource(stub_id);
+        }
+    }
+ 
+    private void queuePhoto(String url, ImageView imageView)
+    {
+        PhotoToLoad p=new PhotoToLoad(url, imageView);
+        executorService.submit(new PhotosLoader(p));
+    }
+ 
+    private Bitmap getBitmap(String url)
+    {
+        File f=fileCache.getFile(url);
+ 
+        //from SD cache
+        Bitmap b = decodeFile(f);
+        if(b!=null)
+            return b;
+ 
+        //from web
+        try {
+            Bitmap bitmap=null;
+            URL imageUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection)imageUrl.openConnection();
+            conn.setConnectTimeout(30000);
+            conn.setReadTimeout(30000);
+            conn.setInstanceFollowRedirects(true);
+            InputStream is=conn.getInputStream();
+            OutputStream os = new FileOutputStream(f);
+            Utils.CopyStream(is, os);
+            os.close();
+            bitmap = decodeFile(f);
+            return bitmap;
+        } catch (Exception ex){
+           ex.printStackTrace();
+           return null;
+        }
+    }
+ 
+    //decodes image and scales it to reduce memory consumption
+    private Bitmap decodeFile(File f){
+        try {
+            //decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f),null,o);
+ 
+            //Find the correct scale value. It should be the power of 2.
+            final int REQUIRED_SIZE=70;
+            int width_tmp=o.outWidth, height_tmp=o.outHeight;
+            int scale=1;
+            while(true){
+                if(width_tmp/2<REQUIRED_SIZE || height_tmp/2<REQUIRED_SIZE)
+                    break;
+                width_tmp/=2;
+                height_tmp/=2;
+                scale*=2;
+            }
+ 
+            //decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize=scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (Exception e) {}
+        return null;
+    }
+ 
+    //Task for the queue
+    private class PhotoToLoad
+    {
+        public String url;
+        public ImageView imageView;
+        public PhotoToLoad(String u, ImageView i){
+            url=u;
+            imageView=i;
+        }
+    }
+ 
+    class PhotosLoader implements Runnable {
+        PhotoToLoad photoToLoad;
+        PhotosLoader(PhotoToLoad photoToLoad){
+            this.photoToLoad=photoToLoad;
+        }
+ 
+        @Override
+        public void run() {
+            if(imageViewReused(photoToLoad))
+                return;
+            Bitmap bmp=getBitmap(photoToLoad.url);
+            memoryCache.put(photoToLoad.url, bmp);
+            if(imageViewReused(photoToLoad))
+                return;
+            BitmapDisplayer bd=new BitmapDisplayer(bmp, photoToLoad);
+            Activity a=(Activity)photoToLoad.imageView.getContext();
+            a.runOnUiThread(bd);
+        }
+    }
+ 
+    boolean imageViewReused(PhotoToLoad photoToLoad){
+        String tag=imageViews.get(photoToLoad.imageView);
+        if(tag==null || !tag.equals(photoToLoad.url))
+            return true;
+        return false;
+    }
+ 
+    //Used to display bitmap in the UI thread
+    class BitmapDisplayer implements Runnable
+    {
+        Bitmap bitmap;
+        PhotoToLoad photoToLoad;
+        public BitmapDisplayer(Bitmap b, PhotoToLoad p){bitmap=b;photoToLoad=p;}
+        public void run()
+        {
+            if(imageViewReused(photoToLoad))
+                return;
+            if(bitmap!=null)
+                photoToLoad.imageView.setImageBitmap(bitmap);
+            else
+                photoToLoad.imageView.setImageResource(stub_id);
+        }
+    }
+ 
+    public void clearCache() {
+        memoryCache.clear();
+        fileCache.clear();
+    }
+ 
+}
+class MemoryCache {
+    private Map<String, SoftReference<Bitmap>> cache=Collections.synchronizedMap(new HashMap<String, SoftReference<Bitmap>>());
+ 
+    public Bitmap get(String id){
+        if(!cache.containsKey(id))
+            return null;
+        SoftReference<Bitmap> ref=cache.get(id);
+        return ref.get();
+    }
+ 
+    public void put(String id, Bitmap bitmap){
+        cache.put(id, new SoftReference<Bitmap>(bitmap));
+    }
+ 
+    public void clear() {
+        cache.clear();
+    }
+}
+class FileCache {
+	 
+    private File cacheDir;
+ 
+    public FileCache(Context context){
+        //Find the dir to save cached images
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
+            cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"LazyList");
+        else
+            cacheDir=context.getCacheDir();
+        if(!cacheDir.exists())
+            cacheDir.mkdirs();
+    }
+ 
+    public File getFile(String url){
+        //I identify images by hashcode. Not a perfect solution, good for the demo.
+        String filename=String.valueOf(url.hashCode());
+        //Another possible solution (thanks to grantland)
+        //String filename = URLEncoder.encode(url);
+        File f = new File(cacheDir, filename);
+        return f;
+ 
+    }
+ 
+    public void clear(){
+        File[] files=cacheDir.listFiles();
+        if(files==null)
+            return;
+        for(File f:files)
+            f.delete();
+    }
+ 
+}
+class Utils {
+    public static void CopyStream(InputStream is, OutputStream os)
+    {
+        final int buffer_size=1024;
+        try
+        {
+            byte[] bytes=new byte[buffer_size];
+            for(;;)
+            {
+              int count=is.read(bytes, 0, buffer_size);
+              if(count==-1)
+                  break;
+              os.write(bytes, 0, count);
+            }
+        }
+        catch(Exception ex){}
+    }
+}
+
